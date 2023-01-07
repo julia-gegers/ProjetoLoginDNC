a969d7ba7f989e463bace44ee2897040cc1f10ba
julia-gegers <juliagegersla@gmail.com>
2023-01-07 16:18:56 -0300

    att

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function check(){
     let cpf = document.getElementById('cpf').value
     let phone = document.getElementById('phone').value
     let password = document.getElementById('password').value 
-    let form = document.getElementById('form')
+    let form = document.getElementById('form').value
+
     let texto1 = document.getElementById('texto1')
     let texto2 = document.getElementById('texto2')
     let texto3 = document.getElementById('texto3')
@@ -14,13 +15,36 @@ function check(){
 
     let text = document.getElementById('text')
 
-    if(form != ''){
-        text.textContent = 'Sucesso'
-        text.style.color = 'green'
-    }
+    if(name, email, cpf, phone, password == ''){
+        texto1.textContent = 'Campo obrigatório'
+        texto1.style.color = 'red'
 
+        texto2.textContent = 'Campo obrigatório'
+        texto2.style.color = 'red'
+
+        texto2.textContent = 'Campo obrigatório'
+        texto2.style.color = 'red'
+
+        texto3.textContent = 'Campo obrigatório'
+        texto3.style.color = 'red'
+
+        texto4.textContent = 'Campo obrigatório'
+        texto4.style.color = 'red'
+
+        texto5.textContent = 'Campo obrigatório'
+        texto5.style.color = 'red'
+
+        text.textContent = 'Campos obrigatórios não registrados'
+        text.style.color = 'red'
+        event.preventDefault()
+    }
     
-       
-       
-    
+ else {
+    text.textContent = 'Sucesso'
+    text.style.color = 'green'
 }
+}
+    
+    
+    
+
